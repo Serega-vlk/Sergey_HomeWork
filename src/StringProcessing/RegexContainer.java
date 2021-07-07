157e3af4cba81f895c76841efe56580f4ca267c9
Serega-vlk <69846480+Serega-vlk@users.noreply.github.com>
2021-07-07 16:32:13 +0300

    String processing: Correct telephone 2 pattern

--- src/StringProcessing/RegexContainer.java
+++ src/StringProcessing/RegexContainer.java
@@ -3,7 +3,7 @@ package StringProcessing;
 public interface RegexContainer {
     String ALL_NAMES_PATTERN = "^[А-ЯA-Z][a-zа-я]+$";
     String TELEPHONE_PATTERN = "^\\+\\d{12}$";
-    String TELEPHONE_PATTERN2 = "\\d{10}$";
+    String TELEPHONE_PATTERN2 = "^\\d{10}$";
     String EMAIL_PATTERN = "^\\w+@\\D+\\.\\D{2,}$";
     String NICKNAME_PATTERN = "^@\\w{4,10}$";
     String BIRTHDAY_PATTERN = "^\\d{2}\\.\\d{2}\\.\\d{4}$";
